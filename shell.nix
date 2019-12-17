dd564cbdee4618107a110b78089ce33b97b09437
Daniel Kahlenberg <573@users.noreply.github.com>
2019-12-17 14:09:43 +0100

    Use pinning in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,2 +1,10 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }:
+{ nixpkgs ? import (builtins.fetchGit {
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-17.09-2019-12-17";
+  url = https://github.com/nixos/nixpkgs-channels;
+  # Commit hash for nixos-unstable as of 2019-12-17
+  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-17.09`
+  rev = "14f9ee66e63077539252f8b4550049381a082518";
+  ref = "nixos-17.09";
+}) {}, compiler ? "ghc7102" }:
 (import ./default.nix { inherit nixpkgs compiler; }).env
