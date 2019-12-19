4d24924b4eb1079bd691374a2f3a1b70ce7c5af0
Daniel Kahlenberg <573@users.noreply.github.com>
2019-12-19 13:31:05 +0100

    Simplify spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,19 +1,9 @@
 {-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 module Main (main) where
 
-import           Test.Hspec
-
 import qualified Database.Redis as Hedis
 
-main :: IO (Either Reply Status)
+main :: IO (Either Hedis.Reply Hedis.Status)
 main = do
   conn <- Hedis.connect Hedis.defaultConnectInfo
-  hspec
-    $ spec conn
-
-spec :: Hedis.Connection -> Spec
-spec conn = do
-  describe "Pings redis-server" $ do
-    it "retrieves the answer Pong" $ do
-      Right answer <- liftIO $ Hedis.runRedis conn ping
-      liftIO $ answer `shouldBe` "Pong"
+  Hedis.runRedis conn Hedis.ping
